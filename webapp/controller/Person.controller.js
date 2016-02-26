6e4a1b0d7812d5c77a3e7137f58ffd90a646d792
stevenspronk <stevenspronk@gmail.com>
2016-02-26 20:48:28 +0000

    routing werkt

--- webapp/controller/Person.controller.js
+++ webapp/controller/Person.controller.js
@@ -3,7 +3,7 @@ sap.ui.define([
 ], function(Controller) {
 	"use strict";
 
-	return Controller.extend("Schoenmaatje.controller.App", {
+	return Controller.extend("Schoenmaatje.controller.Person", {
 
 	});
 
