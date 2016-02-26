73bd68dda6bee3eb9372dee1239d7b46ca6d0bd5
Hermanns Tobias <tobias.hermanns@ordina.nl>
2016-02-26 19:48:07 +0000

    Photo + Scan button toegevoegd + JS

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -4,7 +4,31 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("Schoenmaatje.controller.App", {
+		scanBarcode: function() {
+			cordova.plugins.barcodeScanner.scan(
+				function(result) {
+					console.log(result);
+				},
+				function(error) {
+					alert("Scannen mislukt: " + error);
+				}
+			);
+		},
+		takePhoto: function() {
+			navigator.camera.getPicture(onSuccess, onFail, {
+				quality: 50,
+				destinationType: Camera.DestinationType.FILE_URI
+			});
 
+			function onSuccess(imageURI) {
+				var image = document.getElementById('myImage');
+				image.src = imageURI;
+			}
+
+			function onFail(message) {
+				alert('Failed because: ' + message);
+			}
+		}
 	});
 
 });
